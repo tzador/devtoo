f671f9f05073c8fa96d1f7b112291509ac1f727c
Tim Zadorozhny <tzador@gmail.com>
2023-05-20 08:22:03 +0200

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../normalize.css";
   import "../app.css";
   import { page } from "$app/stores";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <nav>
